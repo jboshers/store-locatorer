19399eded7d75f9138e59d1239798392146f7c6a
Jeff Boshers <boundbystars@gmail.com>
2014-06-09 22:39:06 -0500

    It works on heroku.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+	<title>Pacha Soap Locations!</title>
   {{ HTML::style('css/style.css') }}
 </head>
 <body>
